eff02137095b072bfc4de4c07d2d320dbc50af52
eguitarz <dalema22@gmail.com>
2013-08-18 23:03:19 +0800

    mount .dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,7 +55,7 @@ module.exports = function (grunt) {
           middleware: function (connect) {
             return [
                 lrSnippet,
-                mountFolder(connect, '.tmp'),
+                mountFolder(connect, '.dev'),
                 mountFolder(connect, 'src')
             ];
           }
